3b5c0f76ef8bcedbc7377af56d6b833dd15f22ff
User <user@mbp-de-user.home>
2025-08-01 17:17:43 +0200

    Edit indentation

--- frontend/app/pages/report/index.tsx
+++ frontend/app/pages/report/index.tsx
@@ -4,37 +4,28 @@ import { useParams, useNavigate } from 'react-router-dom';
 
 async function fetchReport(reportId = null) {
   const res = await fetch(`http://localhost:3000/reports/${encodeURIComponent(reportId)}`);
-/*
-  if (!res.ok) {
-	return reply.code(404).send({ error: 'Factory not found' });
-   }
-*/
   const json = await res.json();
   return json;
 }
 
 export function ReportPage() {
   const [factory, setFactory] = useState<IFactory>();
-//  const [loading, setLoading] = useState<boolean>(true);
   const params = useParams();
   const navigate = useNavigate();
   useEffect(() => {
-	if (params.reportId)
-	{
-		fetchReport(params.reportId).then((json) => { 
-			setFactory(() => json.factory);
-//			setLoading(() => false)
-		}).catch(() => {
-		  navigate("/*")
-		});
-	}
+   if (params.reportId)
+    {
+       fetchReport(params.reportId).then((json) => { 
+         setFactory(() => json.factory);
+       }).catch(() => {
+         navigate("/*")
+       });
+     }
   }, [params.reportId]);
 
   return (
-	<div>
-	<p>Coucou, tu veux voir ma div</p>
-	{/*{!loading && <p>Report page for id {factory?.factoryName}.</p>}*/}
-	<p>Report page for id {factory?.factoryName}.</p>
-	</div>
+    <div>
+      <p>Report page for id {factory?.factoryName}.</p>
+    </div>
   );
 }
